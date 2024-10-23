0ae38ffe9e976b830e6d014884f387a5052beda5
sitaramireddytanuboddi <119498640+sitaramireddytanuboddi@users.noreply.github.com>
2024-10-23 14:00:53 +0100

    removed CL 50 scebario, since it was handled in api scope

--- src/test/scala/uk/gov/hmrc/test/api/specs/TfcpAuthErrorValidations.scala
+++ src/test/scala/uk/gov/hmrc/test/api/specs/TfcpAuthErrorValidations.scala
@@ -48,26 +48,6 @@ class TfcpAuthErrorValidations extends BaseSpec with CommonSpec with HttpClient 
         payeeType
       )
     }
-    Scenario(s"Endpoints with a token with insufficient confidence level") {
-      consignorToken = givenGetToken("AAAA12345TFC", 50, "Individual")
-      var response =
-        tfcLink(consignorToken, correlationId, eppUniqueCusId, eppRegRef, AAAAResp.outboundChildPaymentRef, childDOB)
-      thenValidateResponseCode(response, 401)
-      response = tfcBalance(consignorToken, correlationId, eppUniqueCusId, eppRegRef, AAAAResp.outboundChildPaymentRef)
-      thenValidateResponseCode(response, 401)
-      response = tfcPayment(
-        consignorToken,
-        correlationId,
-        eppUniqueCusId,
-        eppRegRef,
-        AAAAResp.outboundChildPaymentRef,
-        paymentAmount,
-        ccpRegReference,
-        ccpPostcode,
-        payeeType
-      )
-      thenValidateResponseCode(response, 401)
-    }
     Scenario(s"Endpoints with a token with confidence level 250") {
       consignorToken = givenGetToken("AAAA12345TFC", 250, "Individual")
       var response =
